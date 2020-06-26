3fcacfad84fdc7c423cc994214bc506900c4ffe4
Shravani Prasad <66654490+pShrav@users.noreply.github.com>
2020-06-26 03:52:14 -0400

    Delete GameplayProject.cpp

--- Source/GameplayProject/GameplayProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GameplayProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameplayProject, "GameplayProject" );
